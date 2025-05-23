eed5738534e6ef1b105f565c9ccbbbec66cc472c
Sensor Dev <dev@geologypi>
2025-05-23 01:48:20 -0400

    Setup sensor wrapper lib and add example service

---
+++ example/logger.py
@@ -0,0 +1,19 @@
+from lib.sensor import read
+from time import sleep
+
+while True:
+    try:
+        sensor_data = read()
+        if sensor_data["sensor_ready"]:
+            print(f"[{sensor_data['last_update']}] Turbidity: {sensor_data['turbidity']}, "
+                f"Temperature: {sensor_data['temperature']}, "
+                f"TDS: {sensor_data['total_dissolved_solids']}, "
+                f"pH: {sensor_data['pH']}")
+        else:
+            print("Sensor not ready. Retrying...")
+    except Exception as e:
+        print(f"Error reading sensor data: {e}")
+
+    # Logs every 10 seconds as a visible example
+    # In reality, the sensor only samples every 5 minutes
+    sleep(10)
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sensor_system",
+    version="0.1.0",
+    packages=find_packages(),
+)
