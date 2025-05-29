2794099c4429e2844a4dbf31ffd81fe81e09006a
Sensor Dev <dev@geologypi>
2025-05-29 11:27:22 -0400

    Change gain value from 1 to 2/3

--- core/sensors/ads_reader.py
+++ core/sensors/ads_reader.py
@@ -5,12 +5,12 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# This class allows sensor classes to import from this module directly
+"""This class allows sensor classes to import from this module directly"""
 
 # Shared I2C and ADS1115 instance
 i2c = busio.I2C(board.SCL, board.SDA)
 _ads = ADS.ADS1115(i2c)
-_ads.gain = 1  # 1 = ±4.096V
+_ads.gain = 2/3  # 1 = ±6.144V
 
 def get_ads():
     """Returns shared ADS1115 instance"""
