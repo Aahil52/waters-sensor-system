6c80e0c6b298559d1400b2de82bc9c11e6bc5665
Sensor Dev <dev@geologypi>
2025-05-29 11:28:14 -0400

    Add TempSensor class and implementation

--- core/sensors/temperature.py
+++ core/sensors/temperature.py
@@ -1,31 +1,30 @@
-# CURRENTLY UNSUPPORTED, CONTINUE ONCE SCREWED IN
-
 import os
 import glob
 import time
 
-class TemperatureSensor():
+class TempSensor:
     def __init__(self):
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
+
         base_dir = '/sys/bus/w1/devices/'
         device_folders = glob.glob(base_dir + '28*')
         if not device_folders:
             raise RuntimeError("No temperature sensors found on OneWire bus.")
         self.device_file = device_folders[0] + '/w1_slave'
 
-    def _read_raw(self) -> list[str]:
+    def read_raw(self):
         with open(self.device_file, 'r') as f:
             return f.readlines()
 
-    def read(self) -> float:
-        lines = self._read_raw()
+    def read(self):
+        lines = self.read_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = self._read_raw()
+            lines = self.read_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos + 2:]
             temp_c = float(temp_string) / 1000.0
-            return temp_c
-        raise RuntimeError("Failed to read temperature data.")
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            return temp_c, temp_f
