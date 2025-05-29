41a5f357fe3719e640009dae454ade50a33402d3
Sensor Dev <dev@geologypi>
2025-05-29 11:34:58 -0400

    Add analog voltage readings for sensor calibration

--- scripts/adc_test.py
+++ scripts/adc_test.py
@@ -4,20 +4,19 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADS object and specify the gain
 ads = ADS.ADS1115(i2c)
-ads.gain = 1 
-turbidity = AnalogIn(ads, ADS.P0)
-ph = AnalogIn(ads, ADS.P1)
-tds = AnalogIn(ads, ADS.P2)
+ads.gain = 2/3
+A0 = AnalogIn(ads, ADS.P0)
+A1 = AnalogIn(ads, ADS.P1)
+A2 = AnalogIn(ads, ADS.P2)
 
 # Continuously print the values
 while True:
-    print(f"Turbidity Voltage: {turbidity.voltage:.2f} V")
-    print(f"pH Voltage: {ph.voltage:.2f} V")
-    print(f"TDS Voltage: {tds.voltage:.2f} V")
+    print(f"A0 Voltage: {A0.voltage:.2f} V")
+    print(f"A1 Voltage: {A1.voltage:.2f} V")
+    print(f"A2 Voltage: {A2.voltage:.2f} V")
     print("-----------------------------")
     time.sleep(1)
